513468512f29240e37c13aa89137b6d1bd133376
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-20 14:40:08 +0900

    src/java/com/codingshuffle/tutorialにconfigurationフォルダとAppConfigファイルを作成する

--- src/main/java/com/codingshuffle/tutorial/configuration/AppConfig.java
+++ src/main/java/com/codingshuffle/tutorial/configuration/AppConfig.java
@@ -1,8 +1,16 @@
 package com.codingshuffle.tutorial.configuration;
 
+import com.codingshuffle.tutorial.DB;
+import com.codingshuffle.tutorial.ProdDB;
+import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Configurable;
 
 @Configurable
 public class AppConfig {
+
+    @Bean
+    public DB getDBBean() {
+        return new ProdDB();
+    }
 
 }
