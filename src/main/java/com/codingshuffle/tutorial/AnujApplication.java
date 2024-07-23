c6615ca0e72586fbab444b402e1616383eb35514
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-24 08:58:23 +0900

    src/java/com/codingshuffle/tutorial/AnujApplicationファイルを編集する

--- src/main/java/com/codingshuffle/tutorial/AnujApplication.java
+++ src/main/java/com/codingshuffle/tutorial/AnujApplication.java
@@ -1,5 +1,6 @@
 package com.codingshuffle.tutorial;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AnujApplication implements CommandLineRunner {
 
+	@Autowired
 	DB db;
 
 	public static void main(String[] args) {
