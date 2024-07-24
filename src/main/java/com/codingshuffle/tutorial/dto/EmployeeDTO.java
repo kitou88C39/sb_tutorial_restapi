fc02ff03d5ce691d84e844c12be5b982575b1e81
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-24 20:03:12 +0900

    src/java/com/codingshuffle/tutorialにdtoフォルダとEmployeeDTOファイルを作成する

--- src/main/java/com/codingshuffle/tutorial/dto/EmployeeDTO.java
+++ src/main/java/com/codingshuffle/tutorial/dto/EmployeeDTO.java
@@ -1,5 +1,11 @@
 package com.codingshuffle.tutorial.dto;
 
+import java.time.LocalDate;
+
 public class EmployeeDTO {
+
+    private Long id;
+    private String name;
+    private LocalDate dateOfJoining;
 
 }
