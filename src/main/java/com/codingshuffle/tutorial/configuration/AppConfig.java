a40c3f267a2ac8b13508f4d2b355d1e42571647e
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-24 14:39:51 +0900

    resources/application.properties を編集する

--- src/main/java/com/codingshuffle/tutorial/configuration/AppConfig.java
+++ src/main/java/com/codingshuffle/tutorial/configuration/AppConfig.java
@@ -5,16 +5,19 @@ import com.codingshuffle.tutorial.DevDB;
 import com.codingshuffle.tutorial.ProdDB;
 import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 @Configurable
 public class AppConfig {
 
     @Bean
+    @ConditionalOnProperty(name = "project.name", havingValue = "development")
     public DB getDevDBBean() {
         return new DevDB();
     }
 
     @Bean
+    @ConditionalOnProperty(name = "project.name", havingValue = "development")
     public DB getProdDBBean() {
         return new ProdDB();
     }
