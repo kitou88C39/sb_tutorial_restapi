d1b5bd79cea49f9a08039f199a29c4eaa1a8a16e
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-24 09:12:06 +0900

    src/java/com/codingshuffle/tutorialにconfiguration/AppConfigファイルを編集する

--- src/main/java/com/codingshuffle/tutorial/configuration/AppConfig.java
+++ src/main/java/com/codingshuffle/tutorial/configuration/AppConfig.java
@@ -1,6 +1,7 @@
 package com.codingshuffle.tutorial.configuration;
 
 import com.codingshuffle.tutorial.DB;
+import com.codingshuffle.tutorial.DevDB;
 import com.codingshuffle.tutorial.ProdDB;
 import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -9,12 +10,12 @@ import org.springframework.beans.factory.annotation.Configurable;
 public class AppConfig {
 
     @Bean
-    public DB getDBBean() {
+    public DB getDevDBBean() {
         return new DevDB();
     }
 
     @Bean
-    public DB getDBBean() {
-        return new DevDB();
+    public DB getProdDBBean() {
+        return new ProdDB();
     }
 }
