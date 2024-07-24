318142f8d5c0f7909a4b4df061558cc9a46a4d14
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-24 20:18:05 +0900

    src/java/com/codingshuffle/tutorialにdtoフォルダとEmployeeDTOファイルを作成する

--- src/main/java/com/codingshuffle/tutorial/dto/EmployeeDTO.java
+++ src/main/java/com/codingshuffle/tutorial/dto/EmployeeDTO.java
@@ -1,8 +1,11 @@
 package com.codingshuffle.tutorial.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class EmployeeDTO {
 
     private Long id;
