d8d2ee3295c37923747da8e0311148eccb1f3e93
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-25 16:42:46 +0900

    src/java/com/codingshuffle/tutorialにentitiesフォルダとEmployeeEntityファイルを作成する

--- src/main/java/com/codingshuffle/tutorial/entities/EmployeeEntity.java
+++ src/main/java/com/codingshuffle/tutorial/entities/EmployeeEntity.java
@@ -1,7 +1,12 @@
 package com.codingshuffle.tutorial.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "employees")
 public class EmployeeEntity {
     private Long id;
     private String name;
