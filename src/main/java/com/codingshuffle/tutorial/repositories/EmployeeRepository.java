c27fdfef3bfe174ea1159b46ea4264ab9a28e3a0
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-25 17:12:27 +0900

    src/java/com/codingshuffle/tutorialにrepositoriesフォルダとEmployeeRepositoryファイルを作成する

--- src/main/java/com/codingshuffle/tutorial/repositories/EmployeeRepository.java
+++ src/main/java/com/codingshuffle/tutorial/repositories/EmployeeRepository.java
@@ -1,5 +1,10 @@
 package com.codingshuffle.tutorial.repositories;
 
-public class EmployeeRepository {
+import com.codingshuffle.tutorial.entities.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 
 }
