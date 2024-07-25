d7f46260e2113fab2aa15782b835a387d529d4d0
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-25 16:57:32 +0900

    src/java/com/codingshuffle/tutorialにentitiesフォルダとEmployeeEntityファイルを作成する

--- src/main/java/com/codingshuffle/tutorial/entities/EmployeeEntity.java
+++ src/main/java/com/codingshuffle/tutorial/entities/EmployeeEntity.java
@@ -1,6 +1,9 @@
 package com.codingshuffle.tutorial.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,6 +17,8 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private LocalDate dateOfJoining;
