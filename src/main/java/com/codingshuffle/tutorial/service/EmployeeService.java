b125e7c2b76c7ecd724b04ae9fa351e975d77e9c
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-25 17:24:08 +0900

    src/java/com/codingshuffle/tutorialにserviceフォルダとEmployeeServiceファイルを作成する

--- src/main/java/com/codingshuffle/tutorial/service/EmployeeService.java
+++ src/main/java/com/codingshuffle/tutorial/service/EmployeeService.java
@@ -1,5 +1,13 @@
 package com.codingshuffle.tutorial.service;
 
+import com.codingshuffle.tutorial.dto.EmployeeDTO;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmployeeService {
+
+    public EmployeeDTO getEmployeeById() {
+
+    }
 
 }
