0f7ab8c6e1bb5909f8d9a5f46199d7714dd2e622
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-25 20:08:26 +0900

    src/java/com/codingshuffle/tutorialにserviceフォルダとEmployeeServiceファイルを作成する

--- src/main/java/com/codingshuffle/tutorial/service/EmployeeService.java
+++ src/main/java/com/codingshuffle/tutorial/service/EmployeeService.java
@@ -1,22 +1,21 @@
 package com.codingshuffle.tutorial.service;
 
 import com.codingshuffle.tutorial.dto.EmployeeDTO;
+import com.codingshuffle.tutorial.entities.EmployeeEntity;
 import com.codingshuffle.tutorial.repositories.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
 
-    @Autowired
     final EmployeeRepository employeeRepository;
 
     public EmployeeService(EmployeeRepository employeeRepository) {
-        this.EmployeeRepository = employeeRepository;
+        this.employeeRepository = employeeRepository;
     }
 
     public EmployeeDTO getEmployeeById(Long id) {
-
+        EmployeeEntity employeeEntity = employeeRepository.getById(id);
     }
 
 }
