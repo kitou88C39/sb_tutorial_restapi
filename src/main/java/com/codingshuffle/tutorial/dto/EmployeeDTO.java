160a776c377d0bc9b3d0241968d50dea03567e4a
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-26 11:00:18 +0900

    src/java/com/codingshuffle/tutorial/dto/EmployeeDTO.javaを編集する

--- src/main/java/com/codingshuffle/tutorial/dto/EmployeeDTO.java
+++ src/main/java/com/codingshuffle/tutorial/dto/EmployeeDTO.java
@@ -1,5 +1,6 @@
 package com.codingshuffle.tutorial.dto;
 
+import com.fasterxml.jackson.annotation.JacksonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,43 +17,4 @@ public class EmployeeDTO {
     private LocalDate dateOfJoining;
     private boolean isActive;
 
-    // public EmployeeDTO(Long id, String name, LocalDate dateOfJoining, boolean
-    // isActive) {
-    // this.id = id;
-    // this.name = name;
-    // this.dateOfJoining = dateOfJoining;
-    // this.isActive = isActive;
-    // }
-
-    // public Long getId() {
-    // return id;
-    // }
-
-    // public void setId(Long id) {
-    // this.id = id;
-    // }
-
-    // public String getName() {
-    // return name;
-    // }
-
-    // public void setName(String name) {
-    // this.name = name;
-    // }
-
-    // public LocalDate getDateOfJoining() {
-    // return dateOfJoining;
-    // }
-
-    // public void setDateOfJoining(LocalDate dateOfJoining) {
-    // this.dateOfJoining = dateOfJoining;
-    // }
-
-    // public boolean isActive() {
-    // return isActive;
-    // }
-
-    // public void setActive(boolean isActive) {
-    // this.isActive = isActive;
-    // }
 }
