f505b6d7d554d542f4b38ead06fa88e1ee04dfc9
kitoumasaomi <kanfutrooper@gmail.com>
2024-07-26 11:37:02 +0900

    src/java/com/codingshuffle/tutorial/service/EmployeeService.javaを編集する

--- src/main/java/com/codingshuffle/tutorial/service/EmployeeService.java
+++ src/main/java/com/codingshuffle/tutorial/service/EmployeeService.java
@@ -4,6 +4,9 @@ import com.codingshuffle.tutorial.dto.EmployeeDTO;
 import com.codingshuffle.tutorial.entities.EmployeeEntity;
 import com.codingshuffle.tutorial.repositories.EmployeeRepository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -28,5 +31,11 @@ public class EmployeeService {
         EmployeeEntity employeeEntity = modelMapper.map(employeeDTO, EmployeeEntity.class);
         return modelMapper.map(employeeRepository.save(employeeEntity), EmployeeDTO.class);
 
+    }
+
+    public List<EmployeeDTO> getAllEmployee() {
+        return employeeRepository.findAll().stream()
+                .map(employeeEntity -> modelMapper.map(employeeEntity, EmployeeDTO.class))
+                .collect(Collectors.toList());
     }
 }
